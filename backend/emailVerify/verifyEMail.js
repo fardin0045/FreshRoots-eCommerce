034671368ecbfafc538e2b90c83224f5c4758bce
fardin0045 <fardinonik40@gmail.com>
2026-06-13 23:08:29 +0600

    smtp

--- backend/emailVerify/verifyEMail.js
+++ backend/emailVerify/verifyEMail.js
@@ -3,9 +3,11 @@ import 'dotenv/config';
 
 export const verifyEmail = (token, email) => {
   const transporter = nodemailer.createTransport({
+    service:'gmail',
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
+    family:4,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
